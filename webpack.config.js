cb1c80f93ed0eef6e07e63d8cd031f084c0d069a
yuchonghua <yuchonghua@163.com>
2017-01-21 15:01:45 +0800

    JavaScript Standard Style
    js标准化

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-var webpack = require("webpack");
-var path = require("path");
+// var webpack = require('webpack')
+var path = require('path')
 
 module.exports = {
-    entry: {
-        "api": "./lib/index.js"
-    },
-    output: {
-        path: path.join(__dirname, './dist'),  //设置打包后的js的输出位置
-        filename: "[name].js"  //和入口文件的名字相同
-    },
-    resolve: {
-        extensions: ['.js', ""],
-        alias: {  //注册模块，以后用的时候可以直接requier("模块名")
-        //    cookie: path.join(__dirname,"./static/js/jquery.cookie.js"),
-        }
-    },
-    plugins:[
-        /*new webpack.optimize.UglifyJsPlugin({
-          output: {
-            comments: false,
-          },
-          compress: {
-            warnings: false
-          }
-      })*/
-    ],
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        }]
-    },
-    babel: {
-        presets: ['es2015']
-    },
-    devtool: "sourcemap"  //生成用来调试的map
+  entry: {
+    'api': './lib/index.js'
+  },
+  output: {
+    path: path.join(__dirname, './dist'),  // 设置打包后的js的输出位置
+    filename: '[name].js'  // 和入口文件的名字相同
+  },
+  resolve: {
+    extensions: ['.js', ''],
+    alias: {  // 注册模块，以后用的时候可以直接requier("模块名")
+    //    cookie: path.join(__dirname,"./static/js/jquery.cookie.js"),
+    }
+  },
+  plugins: [
+    /* new webpack.optimize.UglifyJsPlugin({
+      output: {
+      comments: false,
+      },
+      compress: {
+      warnings: false
+      }
+    }) */
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  babel: {
+    presets: ['es2015']
+  },
+  devtool: 'sourcemap'  // 生成用来调试的map
 }
