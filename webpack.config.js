01c8e3f5f907ae47e28f30630188248c50f7f73f
hadiChan <861883474@qq.com>
2017-01-23 15:25:24 +0800

    dist
    dist

--- webpack.config.js
+++ webpack.config.js
@@ -10,30 +10,24 @@ module.exports = {
     filename: '[name].js'  // 和入口文件的名字相同
   },
   resolve: {
-    extensions: ['.js', ''],
+    extensions: ['.js'],
     alias: {  // 注册模块，以后用的时候可以直接requier("模块名")
     //    cookie: path.join(__dirname,"./static/js/jquery.cookie.js"),
     }
   },
-  plugins: [
-    /* new webpack.optimize.UglifyJsPlugin({
-      output: {
-      comments: false,
-      },
-      compress: {
-      warnings: false
-      }
-    }) */
-  ],
+
   module: {
-    loaders: [{
+    rules: [{
       test: /\.js$/,
+      loader: 'babel-loader',
       exclude: /node_modules/,
-      loader: 'babel-loader'
+      query: {
+        presets: [
+          ['es2015', { modules: false }]
+        ],
+        cacheDirectory: true
+      }
     }]
-  },
-  babel: {
-    presets: ['es2015']
   },
   devtool: 'sourcemap'  // 生成用来调试的map
 }
